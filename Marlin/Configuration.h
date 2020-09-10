9fec0b2581992e21c5b19e8e7dde3960577910db
nfons <natefonseka@Nates-MBP.fios-router.home>
2020-09-10 15:44:41 -0400

    correct offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -56, -14, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { +48, -2, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
