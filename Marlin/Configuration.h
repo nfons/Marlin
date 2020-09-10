ccb40429220b8e8c74432de6830ed40bccc7d3b6
nfons <natefonseka@Nates-MBP.fios-router.home>
2020-09-10 16:16:40 -0400

    PIDs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 16.57
+  #define DEFAULT_Ki 1.33
+  #define DEFAULT_Kd 51.48
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -541,9 +541,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 106.09
+  #define DEFAULT_bedKi 17.64
+  #define DEFAULT_bedKd 425.24
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
